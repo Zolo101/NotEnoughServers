9fe923afbe58e779c0d2de156059baa016a16b75
Zolo101 <umadeguy@gmail.com>
2021-11-30 21:16:54 +0000

    Update to 1.18

--- src/main/java/dev/zelo/java/notenoughservers/NotEnoughServers.java
+++ src/main/java/dev/zelo/java/notenoughservers/NotEnoughServers.java
@@ -1,7 +1,11 @@
 package dev.zelo.java.notenoughservers;
 
 import com.mojang.brigadier.arguments.StringArgumentType;
+//import dev.zelo.java.notenoughservers.commands.JoinserverSuggestor;
+import dev.zelo.java.notenoughservers.mixins.ServerListAccessor;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.client.option.ServerList;
 import net.minecraft.text.LiteralText;
@@ -9,6 +13,9 @@ import net.minecraft.text.Text;
 import net.minecraft.client.MinecraftClient;
 import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.minecraft.util.Formatting;
+
+import java.util.List;
+import java.util.Optional;
 
 public class NotEnoughServers implements ClientModInitializer {
     public static final String MOD_ID = "not-enough-servers";
@@ -19,7 +26,8 @@ public class NotEnoughServers implements ClientModInitializer {
             ClientCommandManager.literal("addserver")
                     .then(ClientCommandManager.argument("ip", StringArgumentType.string())
                             .then(ClientCommandManager.argument("name", StringArgumentType.greedyString())
-                            .executes(context -> {
+                                    .executes(context -> {
+
                                 MinecraftClient client = MinecraftClient.getInstance();
                                 String serverIP = StringArgumentType.getString(context, "ip");
                                 String serverName = StringArgumentType.getString(context, "name");
@@ -48,49 +56,50 @@ public class NotEnoughServers implements ClientModInitializer {
                                 context.getSource().sendFeedback(successText);
                                 return 1;
                             }))));
-            /*
-            ClientCommandManager.literal("joinserver").then(ClientCommandManager.argument("name", StringArgumentType.string()).executes(context -> {
-                Text todoText = new LiteralText("Todo!")
-                        .styled((style -> style.withColor(Formatting.RED)));
-                context.getSource().sendFeedback(todoText);
-                /*
-                MinecraftClient client = MinecraftClient.getInstance();
-                String serverName = StringArgumentType.getString(context, "name");
-                ServerList serverList = new ServerList(client);
-                serverList.loadFile();
-                List<ServerInfo> servers = ((ServerListAccessor) serverList).getServers();
-                Optional<ServerInfo> serverToJoin = servers.stream()
-                        .filter(serverInfo -> serverInfo.name.equals(serverName))
-                        .findFirst();
 
-                if (serverToJoin.isPresent()) {
-                    Text errorText = new LiteralText("Unknown server name!")
-                            .styled((style -> style.withColor(Formatting.RED)));
-//                    client.disconnect(new ConnectScreen(client.currentScreen, client, serverToJoin.get()));
-//                    client.openPauseMenu(true);
-//                    client.gameRenderer.close();
-//                    client.getNetworkHandler().getConnection().handleDisconnection();// .disconnect(new LiteralText("bruh"));
-//                    client.worldRenderer.();
-//                    client.world.disconnect();
-//                    client.disconnect(new SaveLevelScreen(new TranslatableText("menu.savingLevel")));
-//                    client.openScreen(new TitleScreen());
-
+//        ClientCommandManager.DISPATCHER.register(
+//            ClientCommandManager.literal("joinserver")
+//                    .then(ClientCommandManager.argument("name", StringArgumentType.string())
+////                            .suggests(JoinserverSuggestor)
+//                            .executes(context -> {
+//
+//                Text todoText = new LiteralText("Todo!")
+//                        .styled((style -> style.withColor(Formatting.RED)));
+//                context.getSource().sendFeedback(todoText);
+//
+//                MinecraftClient client = MinecraftClient.getInstance();
+//                String serverName = StringArgumentType.getString(context, "name");
+//                ServerList serverList = new ServerList(client);
+//                serverList.loadFile();
+//                List<ServerInfo> servers = ((ServerListAccessor) serverList).getServers();
+//                Optional<ServerInfo> serverToJoin = servers.stream()
+//                        .filter(serverInfo -> serverInfo.name.equals(serverName))
+//                        .findFirst();
+//
+//                if (serverToJoin.isPresent()) {
+//                    Text errorText = new LiteralText("Unknown server name!")
+//                            .styled((style -> style.withColor(Formatting.RED)));
+//
 //                    client.disconnect();
-
-//                    client.getNetworkHandler().getConnection(). .disconnect(new TranslatableText("/joinserver'd"));
-//                    client.currentScreen.removed();
-//                    client.openScreen(new TitleScreen());
-//                    client.openScreen(new TitleScreen());
-//                    client.openScreen(new TitleScreen());
-//                    client.openScreen(new TitleScreen());
-//                    client.openScreen(new ConnectScreen(client.currentScreen, client, serverToJoin.get()));
-                } else {
-                    Text errorText = new LiteralText("Unknown server name!")
-                            .styled((style -> style.withColor(Formatting.RED)));
-
-                    context.getSource().sendFeedback(errorText, false);
-                }
-                return 1;
-            }));*/;
+//                    ConnectScreen.connect(client.currentScreen, client, ServerAddress.parse(serverToJoin.get().address), serverToJoin.get());
+//                } else {
+//                    Text errorText = new LiteralText("Unknown server name!")
+//                            .styled((style -> style.withColor(Formatting.RED)));
+//
+////                    context.getSource().sendFeedback(errorText, false);
+//                }
+//                return 1;
+//            })));
+//        ClientCommandManager.DISPATCHER.register(
+//            ClientCommandManager.literal("relog")
+//                    .executes(context -> {
+//
+//                MinecraftClient client = MinecraftClient.getInstance();
+//                ServerInfo server = client.getCurrentServerEntry();
+//
+//                client.disconnect();
+//                ConnectScreen.connect(client.currentScreen, client, ServerAddress.parse(server.address), server);
+//                return 1;
+//            }));
     }
 }
